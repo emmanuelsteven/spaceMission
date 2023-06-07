9f8d7fa57839b82abb83fae18b017f38787d1c07
emmanuelsteven <write2stevemmy@gmail.com>
2023-06-07 16:23:20 +0100

    Update mission still in progress

--- src/Redux/missions/missionSlice.js
+++ src/Redux/missions/missionSlice.js
@@ -1,21 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+const initialState = {
+    missions: [],
+    isLoading: false,
+    erro: null,
+};
+
+export const getMissions = createAsyncThunk ( 'missions/getMissions', async() => {
+    try{
+        const response = await axios.get('https://api.spacexdata.com/v3/missions');
+        const allMissions = (response.data).map((mission) => mission);
+        return allMissions;
+    } catch (error){
+        throw new Error ('failed to fetch mission data');
+    }
+});
 
 const missionSlice = createSlice({
   name: 'missions',
-  initialState: [],
+  initialState,
   reducers: {
     joinMission: (state, action) => {
-      const newMission = action.payload;
-      state.missions = [...state.missions, newMission];
+      state.missions.push(action.payload);
+    
     },
 
     leaveMission: (state, action) => {
-      const missionId = action.payload;
-      state.missions = state.missions.filter((missions) => missions.id !== missionId);
+      state.missions = state.missions.filter((missions) => missions.id !== action.payload);
     },
+
+
+    extraReducers: (builder) => {
+        builder
+        .addCase(getMissions.pending, (state) => {
+            state.isLoading = true;
+        })
+        .addCase(getMissions.fulfilled, (state, {payload}) => {
+            state.isLoading = false;
+
+        })
+        .addCase(getMissions.rejected, (state) => {
+            state.isLoading = false;
+
+        })
+       
+    }
   },
 });
 
 export const { joinMission, leaveMission } = missionSlice.actions;
-
 export default missionSlice.reducer;
--- src/component/mission.js
+++ src/component/mission.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-const Mission = () => (
-  <div>Mission</div>
-);
 
+const Mission = () => {
+ const dispatch = useDispatch();
+ const missions = useSelector |((state) => state.missions.missions);
+ useEffect(()=>{
+    dispatch(fetchMissionData());
+ }, [dispatch]);
+  return(
+    <table>
+        <thead>
+            <tr>
+                <th>Mission</th>
+                <th>Description</th>
+                <th>Status</th>
+                <th />
+            </tr>
+        </thead>
+        <tbody>
+            {missions.map((mission) =>(
+             <tr key={mission.id}>
+                <td>{mission.mission_name}</td>
+                <td>{mission.description}</td>
+                <td>Not a member</td>
+                <td><button type="button">Join Mission</button></td>
+             </tr>   
+            ))}
+        </tbody>
+    </table>
+  );
+            };
 export default Mission;
