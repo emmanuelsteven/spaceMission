b309ed067f374ba90cf3b2f34c2a60be8d4e1454
BezzB <118984781+BezzB@users.noreply.github.com>
2023-06-07 12:32:36 +0300

    Update Rockets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './component/navBar';
 import Profile from './component/Profile';
+import Rockets from './component/rockets/rockets';
 
 function App() {
   return (
