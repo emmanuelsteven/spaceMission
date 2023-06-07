3ee9a338a921452d1207aa0c8ed407bc636eed44
Emmanuel Steven <114663161+emmanuelsteven@users.noreply.github.com>
2023-06-07 16:44:18 +0100

    Revert "Rockets"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import NavBar from './component/navBar';
-import Profile from './component/Profile';
-import Rockets from './component/rocket/Rockets';
-import { Provider } from 'react-redux';
-import store from './Redux/slices/store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <NavBar />
-        <Routes>
-          {<Route path="/" element={<Rockets />} />}
-          {/* <Route path="/ missions" element={<Missions />} /> */}
-          <Route path="/My-Profile" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <NavBar />
+      {/* <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/ missions" element={<Missions />} />
+        <Route path="/ my profile" element={<MyProfile />} />
+      </Routes> */}
+    </BrowserRouter>
   );
 }
 
--- src/component/navBar.js
+++ src/component/navBar.js
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom';
-import planetImg from './Img/planet.png';
-import './styles/miss.css';
+import './Img/ht.jpg';
 
 const NavBar = () => (
   <nav className="nav-container">
     <div className="nav-logo">
-      <img src={planetImg} alt="navlogo" width={50} height={50} />
+      <img src="Img" alt="navlogo" />
       <h1 className="navtitle">
         <Link to="/">Space Travelers Hub</Link>
       </h1>
     </div>
     <div className="navlinks">
-      <Link to="/" className="navlink">Rockets</Link>
-      <Link to="/mission" className="navlink">Missions</Link>
-      <Link to="/My-Profile" className="navlink   profile">My Profile</Link>
+      <Link to="/">Rockets</Link>
+      <Link to="/mission">Missions</Link>
+      <Link to="/My Profile">MyProfile</Link>
     </div>
   </nav>
 
