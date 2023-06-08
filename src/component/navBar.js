73256190537caeabc098931d74d9cd0f85d918f5
BezzB <118984781+BezzB@users.noreply.github.com>
2023-06-08 17:43:10 +0300

    Update Navbar.js

--- src/component/navBar.js
+++ src/component/navBar.js
@@ -7,16 +7,15 @@ const NavBar = () => (
     <div className="nav-logo">
       <img src={planetImg} alt="navlogo" width={50} height={50} />
       <h1 className="navtitle">
-        <NavLink exact to="/" activeClassName = "active" >Space Travelers Hub</NavLink>
+        <NavLink exact to="/" activeClassName="active">Space Travelers Hub</NavLink>
       </h1>
     </div>
     <div className="navlinks">
       <NavLink to="/" activeClassName="active" className="navlink">Rockets</NavLink>
       <NavLink to="/mission" activeClassName="active" className="navlink">Missions</NavLink>
-      <NavLink to="/Profile" activeClassName="active" className="navlink   profile">My Profile</NavLink>
+      <NavLink to="/Profile" activeClassName="active" className="navlink">My Profile</NavLink>
     </div>
   </nav>
-
 );
 
 export default NavBar;
