f6988793335d65bbb0cc7b20511e34f9c2e9db46
emmanuelsteven <write2stevemmy@gmail.com>
2023-06-08 19:25:36 +0100

    Updated missions

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -2,26 +2,24 @@ import { useSelector } from 'react-redux';
 import './styles/miss.css';
 
 const Profile = () => {
-
-
   const { missions } = useSelector((state) => state.mission);
   const reservedMission = missions.filter((mission) => mission.reserved === true);
 
   return (
-      <div className="mission_reservation">
-        <h2>Space Missions</h2>
-        <div className="mlist-cont">
-          {reservedMission.length === 0 ? (
-            <p>No Missions Scheduled </p>
-          ) : (
-            <ul className="mission-lists">
-              {reservedMission.map((mission) => (
-                <li className="mission-list" key={mission.mission_id}>{mission.mission_name}</li>
-              ))}
-            </ul>
-          )}
-        </div>
+    <div className="mission_reservation">
+      <h2>Space Missions</h2>
+      <div className="mlist-cont">
+        {reservedMission.length === 0 ? (
+          <p>No Missions Scheduled </p>
+        ) : (
+          <ul className="mission-lists">
+            {reservedMission.map((mission) => (
+              <li className="mission-list" key={mission.mission_id}>{mission.mission_name}</li>
+            ))}
+          </ul>
+        )}
       </div>
+    </div>
   );
 };
 
