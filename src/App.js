c681e13816a7230a0bc6dd05833ad620a186a7b3
BezzB <118984781+BezzB@users.noreply.github.com>
2023-06-09 11:33:04 +0300

    Rocket reservation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './component/navBar';
 import Profile from './component/Profile';
 import Mission from './component/mission';
+import Rockets from './component/rocket/Rockets';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
+      {<Route path="/" element={<Rockets />} />}
         <Route path="/Mission" element={<Mission />} />
         <Route path="/My-Profile" element={<Profile />} />
       </Routes>
